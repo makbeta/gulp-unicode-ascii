17041a8b8c1b2d10698a688fa469e27a71c4f46e
makbeta <geek@makbeta.com>
2024-08-13 10:39:14 -0700

    Update to the version
    Support global replacement, support for show output flag

--- index.js
+++ index.js
@@ -1,17 +1,76 @@
 const Stream = require('stream');
 
-function replaceUnicode(source) {
-  const UNICODE_MATCH_REG = /[^\u0000-\u007F]/g;
-  const CONTENT_MATCH_REG = /(?<!-)content\s*:\s*([^;\}]+)/g;
-  const newSource = source.replace(CONTENT_MATCH_REG, (m, p1) => {
-    return m.replace(UNICODE_MATCH_REG, (m) => {
-      return `\\${m.charCodeAt(0).toString(16)}`;
-    });
+/**
+ * Replaces unicode characters with ascii
+ *
+ * @param  {String}  source           input string for replacements
+ * @return {String}                   updated string
+ */
+function replaceUnicodeChars(source, isShowReplacements) {
+  const UNICODE_MATCH_REG = /[^\u0000-\u007F][0-9a-fA-F \t\r\n\f]?/gu;
+  const newSource = source.replace(UNICODE_MATCH_REG, (match) => {
+    let chars = [...match];
+    let codePt = chars[0].codePointAt(0);
+    let out = `\\${codePt.toString(16)}`;
+    if (chars.length > 1) {
+      out = `${out} ${chars[1]}`;
+      if (codePt > 0xfffff) {
+        out = `${out}${chars[1]}`;
+      }
+    }
+    if (isShowReplacements) {
+      console.log(`[${new Date().toLocaleTimeString('it-IT')}]`, `[unicodeToAscii] before: ${match} after: ${out}`);
+    }
+    return out;
   });
+
   return newSource;
 }
 
-module.exports = function unicodeAscii() {
+/**
+ * Replace unicode with ascii in source file
+ * @param  {String}  source           input stream
+ * @param  {Boolean} isCssContentOnly should replacements be made only in css content properties?
+ * @return {String}                   updated stream
+ */
+function replaceUnicode(source, options) {
+  const UNICODE_MATCH_REG = /[^\u0000-\u007F][0-9a-fA-F \t\r\n\f]?/gu;
+  const CONTENT_MATCH_REG = /((?<!-)content\s*:\s*['"]?[a-zA-Z0-9]*)([^\u0000-\u007F][0-9a-fA-F \t\r\n\f]?)([0-9a-fA-F*-_]*['";]+)/gu;
+
+  let isCssContentOnly = false;
+  let isShowReplacements = false;
+
+  if (options?.cssContentOnly) {
+    isCssContentOnly = options.cssContentOnly;
+  }
+
+  if (options?.showReplacements) {
+    isShowReplacements = options.showReplacements;
+  }
+
+  let newSource = source;
+  if (isCssContentOnly) {
+
+    newSource = source.replace(CONTENT_MATCH_REG, (match, p1, p2, p3) => {
+      return `${p1}${replaceUnicodeChars(p2, isShowReplacements)}${p3}`;
+    });
+    console.log(`[${new Date().toLocaleTimeString('it-IT')}]`, '[unicodeToAscii] Replaced in CSS `content` properties only.');
+  } else {
+
+    newSource = replaceUnicodeChars(source, isShowReplacements);
+    console.log(`[${new Date().toLocaleTimeString('it-IT')}]`, '[unicodeToAscii] Replaced everywhere.');
+  }
+
+  return newSource;
+}
+
+/**
+ * Plugin core function, pulls in the Node data stream,
+ * then runs the replacement functions
+ * @param  {object} options          plugin options
+ * @return {Stream}                  stream
+ */
+module.exports = function unicodeToAscii(options) {
   const stream = new Stream.Transform({ objectMode: true });
 
   stream._transform = function (file, encoding, callback) {
@@ -23,12 +82,12 @@ module.exports = function unicodeAscii() {
       const errorOptions = { fileName: file.path, showStack: true };
       const message = 'Streams are not supported';
       setImmediate(() => {
-        callback(new Error('unicode-to-ascii', message, errorOptions));
+        callback(new Error('gulp-unicode-ascii', message, errorOptions));
       });
     }
 
     const contents = String(file.contents);
-    const result = replaceUnicode(contents);
+    const result = replaceUnicode(contents, options);
     file.contents = Buffer.from(result);
 
     setImmediate(() => {
--- readme.md
+++ readme.md
@@ -1,7 +1,20 @@
 # gulp-unicode-ascii
-Converts characters from Unicode to ASCII. Have been tested with SCSS, but can be used with any file.
+Converts characters from Unicode to ASCII. Have been tested with SCSS, but can be used with any file, provided the correct options are psecified.
 
 DartSass compiles all code to Unicode, however, some platforms may not support Unicode. For such cases this plugin has been created.
+
+The plugin should also work when Unicode and ASCII letter & number characters appearing next to each other, it will insert a white space between the two to ensure an entirely different Unicode character is not rendered. For this reason the plugin includes an option to show all replacements in the console.
+
+## Plugin Options
+
+The plugin takes an object of options as an argument. Options are
+
+### `cssContentOnly` - boolean
+Include and set to `true`, if you want the replacement to happen inside the CSS `content` property. 
+Omit or set to false to do the replacements globally.
+
+### `showReplacements` - boolean
+Include and set to `true`, if you would like to see the replacements in the console.
 
 ## How to Use
 For SCSS compilation.
@@ -20,7 +33,7 @@ module.exports = function styles(cb) {
   let stream = gulp.src([`./src/scss/**/*.scss`, `./src/scss/*.scss`]);
 
   stream = stream.pipe(sass.sync(sassOptions))
-    .pipe(unicodeAscii())
+    .pipe(unicodeAscii({ cssContentOnly: true, showReplacements: true }))
     .pipe(concat('styles.css'))
     .pipe(gulp.dest('./dist/css/'));
 
@@ -33,3 +46,6 @@ module.exports = function styles(cb) {
 gulp.task('build', gulp.series(styles));
 ```
 
+
+## Compatibility
+This has been written and tested against Node v20.15.1, NPM 10.7.0 and Gulp v5.0.0. The code uses some ES6 syntax, which can be rewritten, if necessary. 
